397a9fd58d626e65df05ad7268eaed85e32d79dd
Dencho96 <denysvk.96@gmail.com>
2020-10-02 01:42:30 +0300

    added fixes/added few drop-downs

--- app/js/main.js
+++ app/js/main.js
@@ -6,38 +6,45 @@ var mySwiper = new Swiper('.features__slider', {
         delay: 7000,
     },
     navigation: {
-        nextEl: '.features__slide-next',
-        prevEl: '.features__slide-prev',
+        nextEl: '.features__btn--next',
+        prevEl: '.features__btn--prev',
     },
 
 })
 
 var mySwiperFeed = new Swiper('.followers-feed__slider', {
-    slidesPerView: 1,
+    slidesPerView: 3,
     direction: 'horizontal',
     loop: true,
     navigation: {
-        nextEl: '.followers-feed__slide-next',
-        prevEl: '.followers-feed__slide-prev',
+        nextEl: 'followers-feed__btn--prev',
+        prevEl: 'followers-feed__btn--prev',
     },
-
 })
 
+var feedback = new Swiper('.feedback__slider', {
+    slidesPerView: 2,
+    direction: 'horizontal',
+    loop: true,
+    navigation: {
+        nextEl: 'feedback__nav-btn--next',
+        prevEl: 'feedback__nav-btn--prev',
+    },
+})
 
 $(function() {
 
-    $(".product__rating").rateYo({
+    $('.product__rating').rateYo({
         rating: 5,
         starWidth: "15px",
-        spacing: "3px"
+        spacing: "3px",
+        ratedFill: "#FFC000",
     });
-
-    $(".product__rating--sm").rateYo({
-        rating: 5,
-        starWidth: "13px",
-        spacing: "3px"
-    });
-
 
     var mixer = mixitup('.newest__product-inner');
+
+    $('.block-intro__filter-by').on('click', function() {
+        $('.block-intro__menu').toogleClass(' d-block');
+    });
+
 });
